c3290dc8debc506987b88e2a0d891f548c4fb5c9
pablobernabeu <pablobernabeu@users.noreply.github.com>
2026-05-08 21:38:32 +0000

    Deploy: 08052026 pablobernabeu/website-files@82789f0f9f03848834fbfc65c60c2d1ffef34bca

--- publication/secure-local-hpc-speech-transcription/related references/related references.R
+++ publication/secure-local-hpc-speech-transcription/related references/related references.R
@@ -12,35 +12,35 @@ library(rscopus)
 
 query = 
   paste(
-    '"Secure and scalable speech transcription for local and HPC" OR ',
-    '"Learn OpenAI Whisper: Transform your understanding of GenAI through robust and accurate speech processing solutions" OR ',
+    'TITLE("Secure and scalable speech transcription for local and HPC") OR ',
+    'TITLE("Learn OpenAI Whisper: Transform your understanding of GenAI through robust and accurate speech processing solutions") OR ',
     # Titles from https://scholar.google.com/scholar?hl=en&as_sdt=0%2C5&q=whisper+openai+speech+transcription&btnG=
-    '"Leveraging openai whisper model to improve speech recognition for dysarthric individuals" OR ',
-    '"Quantization for OpenAI\'s Whisper Models: A Comparative Analysis" OR ',
-    '"Fine-Tuning OpenAI Whisper and DistilWhisper: An In-Depth Analysis" OR ',
-    '"Moonshine: Speech recognition for live transcription and voice commands" OR ',
-    '"Automated Speech-To-Text Captioning for Videos and Noise Robustness Analysis Using OpenAI Whisper: A Performance and Enhancement Study" OR ',
-    '"Evaluating automatic transcription models utilising cloud platforms" OR ',
-    '"Efficient and accurate transcription in mental health research: A tutorial on using whisper AI for audio file transcription" OR ',
+    'TITLE("Leveraging openai whisper model to improve speech recognition for dysarthric individuals") OR ',
+    'TITLE("Quantization for OpenAI\'s Whisper Models: A Comparative Analysis") OR ',
+    'TITLE("Fine-Tuning OpenAI Whisper and DistilWhisper: An In-Depth Analysis") OR ',
+    'TITLE("Moonshine: Speech recognition for live transcription and voice commands") OR ',
+    'TITLE("Automated Speech-To-Text Captioning for Videos and Noise Robustness Analysis Using OpenAI Whisper: A Performance and Enhancement Study") OR ',
+    'TITLE("Evaluating automatic transcription models utilising cloud platforms") OR ',
+    'TITLE("Efficient and accurate transcription in mental health research: A tutorial on using whisper AI for audio file transcription") OR ',
     # Titles from https://scholar.google.com/scholar?start=10&q=whisper+openai+speech+transcription&hl=en&as_sdt=0,5
-    '"What automatic speech recognition can and cannot do for conversational speech transcription" OR ',
-    '"Comparing Speech-to-Text Algorithms for Transcribing Voice Data from Surveys" OR ',
-    '"Transcribing and coding voice answers obtained in web surveys: comparing three leading automatic speech recognition tools and human versus LLM-based coding" OR ',
-    '"Everyday conversations: a comparative study of expert transcriptions and ASR outputs at a lexical level" OR ',
-    '"Transcription of Audio and Video with OpenAI\'s Whisper" OR ',
-    '"Cantonese Dialect Transcription in Diverse Sophisticated Scenarios via the OpenAI Whisper Speech Recognition Model" OR ',
-    '"Speech Recognition and Accessibility in Panopto Videos: A Comparative Analysis of OPENAI\'s Whisper and Traditional Automatic Speech Recognition" OR ',
-    '"AI-Powered Audio Summarization and Ethical Content Analysis Using OpenAI Whisper" OR ',
+    'TITLE("What automatic speech recognition can and cannot do for conversational speech transcription") OR ',
+    'TITLE("Comparing Speech-to-Text Algorithms for Transcribing Voice Data from Surveys") OR ',
+    'TITLE("Transcribing and coding voice answers obtained in web surveys: comparing three leading automatic speech recognition tools and human versus LLM-based coding") OR ',
+    'TITLE("Everyday conversations: a comparative study of expert transcriptions and ASR outputs at a lexical level") OR ',
+    'TITLE("Transcription of Audio and Video with OpenAI\'s Whisper") OR ',
+    'TITLE("Cantonese Dialect Transcription in Diverse Sophisticated Scenarios via the OpenAI Whisper Speech Recognition Model") OR ',
+    'TITLE("Speech Recognition and Accessibility in Panopto Videos: A Comparative Analysis of OPENAI\'s Whisper and Traditional Automatic Speech Recognition") OR ',
+    'TITLE("AI-Powered Audio Summarization and Ethical Content Analysis Using OpenAI Whisper") OR ',
     # Titles from https://scholar.google.com/scholar?start=20&q=whisper+openai+speech+transcription&hl=en&as_sdt=0,5
-    '"Assessing Whisper automatic speech recognition and WER scoring for elicited imitation: Steps toward automation" OR ',
-    '"Careless whisper: Speech-to-text hallucination harms" OR ',
-    '"Automated transcription of interviews in qualitative research using artificial intelligence: a simple guide" OR ',
-    '"Optimizing speech recognition for medical transcription: fine-tuning whisper and developing a web application" OR ',
-    '"Evaluating AI Speech and Text Technologies to Reduce the Administrative Burden in Occupational Health: An Exploratory Study on Whisper and ChatGPT-4" OR ',
-    '"Maithili Speech Recognition with OpenAI\'s Whisper: A Fine-Tuning Approach" OR ',
-    '"Lyricwhiz: Robust multilingual zero-shot lyrics transcription by whispering to chatgpt" OR ',
-    '"Enhancing Diversity in Inclusive Learning Classroom Using OpenAI Whisper Model" OR ',
-    '"Automatic speech recognition and the transcription of indistinct forensic audio: how do the new generation of systems fare?"'
+    'TITLE("Assessing Whisper automatic speech recognition and WER scoring for elicited imitation: Steps toward automation") OR ',
+    'TITLE("Careless whisper: Speech-to-text hallucination harms") OR ',
+    'TITLE("Automated transcription of interviews in qualitative research using artificial intelligence: a simple guide") OR ',
+    'TITLE("Optimizing speech recognition for medical transcription: fine-tuning whisper and developing a web application") OR ',
+    'TITLE("Evaluating AI Speech and Text Technologies to Reduce the Administrative Burden in Occupational Health: An Exploratory Study on Whisper and ChatGPT-4") OR ',
+    'TITLE("Maithili Speech Recognition with OpenAI\'s Whisper: A Fine-Tuning Approach") OR ',
+    'TITLE("Lyricwhiz: Robust multilingual zero-shot lyrics transcription by whispering to chatgpt") OR ',
+    'TITLE("Enhancing Diversity in Inclusive Learning Classroom Using OpenAI Whisper Model") OR ',
+    'TITLE("Automatic speech recognition and the transcription of indistinct forensic audio: how do the new generation of systems fare?")'
   )
 
 search_period = '2020-2026'
